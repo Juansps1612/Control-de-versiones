d7794446717948f5f088a06c29f1f9c62fc9a50a
Juan Sebastián Pinzón Sánchez <jspinzons@academia.usbbog.edu.co>
2025-08-23 18:45:46 -0500

    Añadir función multiplicar

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,10 @@ def sumar(a, b):
 def restar(a, b):
     return a - b
 
+def multiplicar(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print("Suma de 5 + 3 =", sumar(5, 3))
     print("Resta de 5 - 3 =", restar(5, 3))
+    print("Multiplicación de 5 * 3 =", multiplicar(5, 3))
