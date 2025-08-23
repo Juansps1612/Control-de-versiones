93c9dcb5cd6d88f4e7a3e41c6e232b352dfc6278
Juan Sebastián Pinzón Sánchez <jspinzons@academia.usbbog.edu.co>
2025-08-23 18:43:21 -0500

    Añadir función restar

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,9 @@
 def sumar(a, b):
     return a + b
 
+def restar(a, b):
+    return a - b
 
 if __name__ == "__main__":
     print("Suma de 5 + 3 =", sumar(5, 3))
+    print("Resta de 5 - 3 =", restar(5, 3))
